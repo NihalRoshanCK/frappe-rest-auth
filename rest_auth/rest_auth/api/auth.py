8cf2182aa6e99f0aad782a755d39cbf4d45d3384
Nihalroshan55 <nihalroshan55@gmail.com>
2024-11-28 18:55:17 +0530

    update

--- rest_auth/rest_auth/api/auth.py
+++ rest_auth/rest_auth/api/auth.py
@@ -9,7 +9,8 @@ def login(username, password):
 
         if authenticated_user:
             # Generate API Key and Secret
-            api_key , api_secret =  frappe.generate_auth_token(authenticated_user)
+            api_key, api_secret = generate_keys(authenticated_user)
+
             # Fetch fresh data for the response
             user_doc = frappe.get_doc('User', authenticated_user)
             user_doc.reload()  # Reload to get updated data
@@ -18,8 +19,8 @@ def login(username, password):
             frappe.response["message"] = {
                 "success_key": 1,
                 "message": "Authentication successful",
-                "api_key": api_key,
-                "api_secret": api_secret,
+                "api_key": api_key,  # Use the generated api_key
+                "api_secret": api_secret,  # Use the generated api_secret
                 "user": authenticated_user
             }
         else:
@@ -67,4 +68,5 @@ def generate_keys(user):
     frappe.logger().info(f"New API Key: {user_details.api_key}, API Secret: {user_details.api_secret}")
     print(f"New API Key: {user_details.api_key}, API Secret: {user_details.api_secret}")
 
-    return api_secret
+    # Return both API key and API secret
+    return api_key, api_secret
