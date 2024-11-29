746e25c877d762c0c504b6b971c4ba09b126edc2
Nihalroshan55 <nihalroshan55@gmail.com>
2024-11-29 15:52:03 +0530

    update

--- rest_auth/rest_auth/api/auth.py
+++ rest_auth/rest_auth/api/auth.py
@@ -1,33 +1,43 @@
 import frappe
 from frappe.auth import LoginManager
 
-@frappe.whitelist(allow_guest = True)
-def login(username,password):
-	login_manager = LoginManager()
-	login_manager.authenticate(username,password)
-	login_manager.post_login()
-	if frappe.response['message'] == 'Logged In':
-		user = login_manager.user
-		frappe.response['key_details'] = generate_key(user)
-		frappe.response['user_details'] = get_user_details(user)
-	else:
-		return False
-	
+@frappe.whitelist(allow_guest=True)
+def login(username, password):
+    try:
+        login_manager = LoginManager()
+        login_manager.authenticate(username, password)
+        login_manager.post_login()
+
+        # Explicitly set the response message
+        frappe.response['message'] = 'Logged In'
+        
+        # Generate API keys and user details
+        user = login_manager.user
+        frappe.response['key_details'] = generate_key(user)
+        frappe.response['user_details'] = get_user_details(user)
+
+    except frappe.exceptions.AuthenticationError:
+        frappe.response['message'] = 'Invalid login'
+        return False
+
 def generate_key(user):
-	user_details = frappe.get_doc("User", user)
-	api_secret = api_key = ''
-	if not user_details.api_key and not user_details.api_secret:
-		api_secret = frappe.generate_hash(length=15)
-		api_key = frappe.generate_hash(length=15)
-		user_details.api_key = api_key
-		user_details.api_secret = api_secret
-		user_details.save(ignore_permissions = True)
-	else:
-		api_secret = user_details.get_password('api_secret')
-		api_key = user_details.get('api_key')
-	return {"api_secret": api_secret,"api_key": api_key}
+    user_details = frappe.get_doc("User", user)
+    api_secret = api_key = ''
+    if not user_details.api_key and not user_details.api_secret:
+        api_secret = frappe.generate_hash(length=15)
+        api_key = frappe.generate_hash(length=15)
+        user_details.api_key = api_key
+        user_details.api_secret = api_secret
+        user_details.save(ignore_permissions=True)
+    else:
+        api_secret = user_details.get_password('api_secret')
+        api_key = user_details.get('api_key')
+    return {"api_secret": api_secret, "api_key": api_key}
 
 def get_user_details(user):
-	user_details = frappe.get_all("User",filters={"name":user},fields=["name","first_name","last_name","email","mobile_no","gender","role_profile_name"])
-	if user_details:
-		return user_details
+    user_details = frappe.get_all(
+        "User",
+        filters={"name": user},
+        fields=["name", "first_name", "last_name", "email", "mobile_no", "gender", "role_profile_name"]
+    )
+    return user_details if user_details else {}
